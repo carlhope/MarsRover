5b3aab055c6bfa2910d1709d43f251156d557336
Carl Hope <mail.carlhope@gmail.com>
2024-12-05 19:41:36 +0000

    add position.cs

--- MarsRover/Models/InputLayer/Position.cs
+++ MarsRover/Models/InputLayer/Position.cs
@@ -2,5 +2,8 @@
 
 public class Position
 {
-
+// Perhaps these should be public auto-properties, perhaps private fields... it's up to you!
+	int x;
+	int y;
+	CompassDirection facing; // this type can be whatever your direction enum is called
 }
